95547ce109daa5b52ea93dfefcf6bc0c597507ab
brianechavarria <102111950+brianechavarria@users.noreply.github.com>
2024-01-16 19:16:22 -0500

    Update foodScraper.py

--- foodScraper.py
+++ foodScraper.py
@@ -3,8 +3,15 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-base = "https://dining.columbia.edu/"
+urls = ["https://dining.columbia.edu/content/john-jay-dining-hall",
+        "https://dining.columbia.edu/content/jjs-place-0",
+        "https://dining.columbia.edu/content/ferris-booth-commons-0",
+        "https://dining.columbia.edu/chef-mikes",
+        "https://dining.columbia.edu/content/faculty-house-0",
+        "https://dining.columbia.edu/content/chef-dons-pizza-pi",
+        "https://dining.columbia.edu/content/grace-dodge-dining-hall-0",
+        "https://dining.columbia.edu/content/fac-shack"]
 
-url = base 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, features="lxml")
+for url in urls:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, features="lxml")
