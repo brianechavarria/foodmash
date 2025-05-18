e6162a17696b9a64adb88d408a6c731e5e07ab78
brianechavarria <brianechavarria7@gmail.com>
2025-05-18 16:08:43 -0500

    reworking elo function

--- elo_calc.py
+++ elo_calc.py
@@ -2,17 +2,27 @@
 
 import math
 
-constant = 30
 
-class Elo(object):
 
-    def match(self, winner, loser):
-    
-        winner_prob = 1 * 1 / (1 + 1 * math.pow(10, 1 * (winner.rating - loser.rating) / 400))
-        loser_prob = 1 * 1 / (1 + 1 * math.pow(10, 1 * (winner.rating - loser.rating) / 400))
+class meal:
 
-        winner.rating = winner.rating + constant * (1 - winner_prob)
-        winner.rating = winner.rating + constant * (0 - loser_prob)
+    def __init__(self, image, name):
+        self.image=image #points to image url
+        self.name=name #name of the meal
+        self.elo=800 #beginner chess elo
+
+def match(winner, loser):
+
+    constant = 30
+
+    winner_prob = 1 * 1 / (1 + 1 * math.pow(10, 1 * (winner.elo - loser.elo) / 400))
+    loser_prob = 1 * 1 / (1 + 1 * math.pow(10, 1 * (loser.elo - winner.elo) / 400))
+
+    winner.elo = winner.elo + constant * (1 - winner_prob)
+    loser.elo = loser.elo + constant * (0 - loser_prob)
+
+    print(winner.elo)
+    print(loser.elo)
 
 #adding a comment 
 test = 1
